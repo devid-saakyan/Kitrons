e1a22d4800b54fb0c050c4733c36581f0135a85f
work_penguin <d.sahakyan10@gmail.com>
2024-09-27 17:19:32 +0400

    settings

--- Kitrons/settings.py
+++ Kitrons/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'account',
     'ads',
@@ -52,7 +53,11 @@ INSTALLED_APPS = [
     'drf_yasg',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
